47d1ac87357a922c471cabcf7e5f69d2363ec591
Jonnesus <bi24fz@student.sunderland.ac.uk>
2023-11-04 18:44:47 +0000

    V0.5.1

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
@@ -7,20 +8,29 @@ public class MainMenuController : MonoBehaviour
 
     [Tooltip("Drag in an options menu panel, if one exists")]
     [SerializeField] GameObject optionsMenuPanel;
+    [SerializeField] GameObject optionsCloseButton;
+    [SerializeField] GameObject optionsOpenButton;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     public void OptionsMenuClose()
     {
+        EventSystem.current.SetSelectedGameObject(optionsOpenButton);
         optionsMenuPanel.SetActive(false);
     }
 
     public void OptionsMenuOpen()
     {
         optionsMenuPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(optionsCloseButton);
     }
 
     public void StartGame()
     {
-        Cursor.visible = false;
         SceneManager.LoadScene(playScene);
     }
 
